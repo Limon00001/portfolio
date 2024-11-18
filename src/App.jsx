04126f4725012ef2da61ca729461e6c4bc9907a2
Limon00001 <limonhossain82@gmail.com>
2024-11-18 21:38:17 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        Navbar
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
